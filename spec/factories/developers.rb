cea72255184abde9a4b25d23d9a1051cd21578f0
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2022-01-30 21:53:53 +0700

    init dev&prod

---
+++ spec/factories/developers.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :developer do
+    title { "MyString" }
+    bytitle { "MyString" }
+    img { "MyString" }
+    description { "MyString" }
+  end
+end
---
+++ spec/factories/products.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :product do
+    title { "MyString" }
+    bytitle { "MyString" }
+    content { "MyText" }
+    price { 1.5 }
+    old_price { 1 }
+    status { 1 }
+    keywords { "MyString" }
+    description { "MyString" }
+    img { "MyString" }
+    hit { 1 }
+  end
+end
